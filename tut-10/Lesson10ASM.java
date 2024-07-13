efe2cac28c978ea9a2f60df6d4000d3ca5332f34
Bruce Luk <lukusto@gmail.com>
2024-07-14 00:43:42 +0800

    Update: 10 assignment

--- tut-10/Lesson10ASM.java
+++ tut-10/Lesson10ASM.java
@@ -6,7 +6,7 @@ import javax.swing.JLabel;
 class Ball extends JLabel {
   public int CoordX = 0;
   private final int COORD_Y;
-  public static int TwoRadius = 50;
+  public static final int DIAMETER = 50;
   public static int LabelWidth = 1000;
   public static int LabelHeight = 1000;
 
@@ -20,25 +20,23 @@ class Ball extends JLabel {
     g.fillRect(0, COORD_Y, LabelWidth, LabelHeight);
 
     g.setColor(Color.RED);
-    g.fillOval(CoordX, COORD_Y, TwoRadius, TwoRadius);
+    g.fillOval(CoordX, COORD_Y, DIAMETER, DIAMETER);
   }
 }
 
 class Bounce implements Runnable {
   private final Ball BALL;
-  public static int MAX_FRAME_WIDTH;
-  private int DELAY_INTERVAL = 1;
+  public static int MaxBounceWidth;
+  private final int DELAY_INTERVAL;
 
-  public Bounce(Ball ball, int DELAY_INTERVAL) {
+  public Bounce(Ball ball, int delayInterval) {
     this.BALL = ball;
-    if (DELAY_INTERVAL > 0) {
-      this.DELAY_INTERVAL = DELAY_INTERVAL;
-    }
+    this.DELAY_INTERVAL = delayInterval > 0 ? delayInterval : 1;
   }
 
   public void run() {
     while (true) {
-      for (int i = 0; i < MAX_FRAME_WIDTH - Ball.TwoRadius; i++) {
+      for (int i = 0; i < MaxBounceWidth - Ball.DIAMETER; i++) {
         BALL.CoordX = i;
         BALL.repaint();
 
@@ -48,7 +46,7 @@ class Bounce implements Runnable {
         }
       }
 
-      for (int i = MAX_FRAME_WIDTH - Ball.TwoRadius; i > 0; i--) {
+      for (int i = MaxBounceWidth - Ball.DIAMETER; i > 0; i--) {
         BALL.CoordX = i;
         BALL.repaint();
 
@@ -59,30 +57,35 @@ class Bounce implements Runnable {
       }
     }
   }
-
 }
 
 public class Lesson10ASM {
+  // Some settings
   private static final int DELAY_INTERVAL = 1;
   private static final int MAX_FRAME_WIDTH = 600;
-  private static final int NO_OF_BALLS = 12;
+  private static int NoOfBalls = 14;
 
   public static void main(String[] args) {
-    final int MAX_HEIGHT_FOR_BALL = MAX_FRAME_WIDTH / NO_OF_BALLS;
+    final int MAX_NO_OF_BALLS = MAX_FRAME_WIDTH / Ball.DIAMETER;
+
+    if (NoOfBalls > MAX_NO_OF_BALLS) {
+      NoOfBalls = MAX_NO_OF_BALLS;
+    }
+
+    final int MAX_HEIGHT_FOR_BALL_BOUNCING = MAX_FRAME_WIDTH / NoOfBalls;
     Ball.LabelWidth = MAX_FRAME_WIDTH;
-    Ball.LabelHeight = MAX_HEIGHT_FOR_BALL;
+    Ball.LabelHeight = MAX_HEIGHT_FOR_BALL_BOUNCING;
+    Bounce.MaxBounceWidth = MAX_FRAME_WIDTH;
 
     JFrame frame = new JFrame("Bouncing Balls");
     frame.setSize(MAX_FRAME_WIDTH, MAX_FRAME_WIDTH);
     frame.setLayout(null);
 
-    for (int i = 0; i < NO_OF_BALLS; i++) {
-      Ball ball = new Ball(i * MAX_HEIGHT_FOR_BALL);
-      ball.setLocation(0, 0);
+    for (int i = 0; i < NoOfBalls; i++) {
+      Ball ball = new Ball(i * MAX_HEIGHT_FOR_BALL_BOUNCING);
       ball.setSize(MAX_FRAME_WIDTH, MAX_FRAME_WIDTH);
       frame.add(ball);
 
-      Bounce.MAX_FRAME_WIDTH = MAX_FRAME_WIDTH;
       Bounce bounce = new Bounce(ball, DELAY_INTERVAL * (i + 1));
       Thread thread = new Thread(bounce);
       thread.start();
